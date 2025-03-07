242c5171ecb605284799fecb7bc964831ab5a14b
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2025-03-07 16:54:26 +0100

    chore: Install phpcs dev dependency, cleanup functions.php

--- functions.php
+++ functions.php
@@ -8,29 +8,46 @@
 /**
  * Stylesheets
  *
- * Parent and child theme stylesheet as well as a stylesheet 
+ * Parent and child theme stylesheet as well as a stylesheet
  * for the gutenberg editor.
  */
 
 // Parent stylesheet and custom frontent stylesheet.
-add_action('wp_enqueue_scripts', 'morntag_child_hello_elementor_enqueue_styles');
+add_action( 'wp_enqueue_scripts', 'morntag_child_hello_elementor_enqueue_styles' );
 
 function morntag_child_hello_elementor_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style(
-        'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+	$parent_style_uri = get_template_directory_uri() . '/style.css';
+	$parent_style_dir = get_template_directory() . '/style.css';
+	$child_style_uri  = get_stylesheet_directory_uri() . '/style.css';
+	$child_style_dir  = get_stylesheet_directory() . '/style.css';
+
+	wp_enqueue_style(
+		'parent-style',
+		$parent_style_uri,
+		array(),
+		filemtime( $parent_style_dir )
+	);
+
+	wp_enqueue_style(
+		'child-style',
+		$child_style_uri,
+		array( 'parent-style' ),
+		filemtime( $child_style_dir )
+	);
 }
 
 // Editor stylesheet (backend).
-add_action('enqueue_block_editor_assets', 'morntag_editor_style');
+add_action( 'enqueue_block_editor_assets', 'morntag_editor_style' );
 
 function morntag_editor_style() {
-  wp_enqueue_style(
-    'morntag-editor-style',
-    get_stylesheet_directory_uri() . '/assets/css/editor-style.css');
+	$editor_style_uri = get_stylesheet_directory_uri() . '/assets/css/editor-style.css';
+	$editor_style_dir = get_stylesheet_directory() . '/assets/css/editor-style.css';
+	wp_enqueue_style(
+		'morntag-editor-style',
+		$editor_style_uri,
+		array(),
+		filemtime( $editor_style_dir )
+	);
 }
 
 
@@ -40,28 +57,28 @@ function morntag_editor_style() {
  * Uncomment the hook to activate the respective script.
  */
 
-// Frontend script
+/** Frontend script */
 function morntag_enqueue_frontend_scripts() {
-    wp_enqueue_script(
-        'morntag-frontend-script',
-        get_stylesheet_directory_uri() . '/assets/js/frontend.js',
-        array(), 
-        '1.0.0',
-        true 
-    );
+	wp_enqueue_script(
+		'morntag-frontend-script',
+		get_stylesheet_directory_uri() . '/assets/js/frontend.js',
+		array(),
+		'1.0.0',
+		true
+	);
 }
 
 // add_action('wp_enqueue_scripts', 'morntag_enqueue_frontend_scripts');
 
-// Admin script
+/** Admin script */
 function morntag_enqueue_admin_scripts() {
-    wp_enqueue_script(
-        'morntag-admin-script',
-        get_stylesheet_directory_uri() . '/assets/js/admin.js',
-        array(), 
-        '1.0.0',
-        true 
-    );
+	wp_enqueue_script(
+		'morntag-admin-script',
+		get_stylesheet_directory_uri() . '/assets/js/admin.js',
+		array(),
+		'1.0.0',
+		true
+	);
 }
 
 // add_action('admin_enqueue_scripts', 'morntag_enqueue_admin_scripts');
